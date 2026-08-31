aec4e2b74573aa1e77af36ccc7d0218a3e7b1e3e
Asela054 <asela.indrajith@gmail.com>
2026-08-31 11:19:22 +0530

    Update on 31/08/2026
    GRN voucher update material issue table
    Issue note set materail issue table date
    Login page apply company name & branch
    Manu bar set company information

--- application/models/GRNVoucherinfo.php
+++ application/models/GRNVoucherinfo.php
@@ -778,17 +778,23 @@ class GRNVoucherinfo extends CI_Model{
                     $this->db->update('tbl_print_stock', $dataupdatestock);
 
                     //Update job card issue material unit price update
-                    $dataupdateissuematerial = array(
-                        'tbl_jobcard_issue_meterial.unitprice' => $rowaftercostdata->costunitprice,
-                        'tbl_jobcard_issue_meterial.updateuser' => $userID,
-                        'tbl_jobcard_issue_meterial.updatedatetime' => $updatedatetime
-                    );
-                    $this->db->join('tbl_jobcard', 'tbl_jobcard.idtbl_jobcard = tbl_jobcard_issue_meterial.tbl_jobcard_idtbl_jobcard', 'left');
-                    $this->db->where('tbl_jobcard_issue_meterial.tbl_print_material_info_idtbl_print_material_info', $rowaftercostdata->tbl_print_material_info_idtbl_print_material_info);
-                    $this->db->where('tbl_jobcard.tbl_company_idtbl_company', $company);
-                    $this->db->where('tbl_jobcard.tbl_company_branch_idtbl_company_branch', $branch);
-                    $this->db->where('tbl_jobcard_issue_meterial.batchno', $grnData->batchno);
-                    $this->db->update('tbl_jobcard_issue_meterial', $dataupdateissuematerial);
+                    $sqlupdateissuematerial = "UPDATE tbl_jobcard_issue_meterial jim
+                            INNER JOIN tbl_jobcard jc ON jc.idtbl_jobcard = jim.tbl_jobcard_idtbl_jobcard
+                            SET jim.unitprice = ?, jim.updateuser = ?, jim.updatedatetime = ?
+                            WHERE jim.tbl_print_material_info_idtbl_print_material_info = ?
+                            AND jc.tbl_company_idtbl_company = ?
+                            AND jc.tbl_company_branch_idtbl_company_branch = ?
+                            AND jim.batchno = ?";
+
+                    $this->db->query($sqlupdateissuematerial, array(
+                        $rowaftercostdata->costunitprice,
+                        $userID,
+                        $updatedatetime,
+                        $rowaftercostdata->tbl_print_material_info_idtbl_print_material_info,
+                        $company,
+                        $branch,
+                        $grnData->batchno
+                    ));
                 }
     
                 // Update GRN with cost details
